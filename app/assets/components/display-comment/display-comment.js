127ea8bf2319ca2e617fffe0a966f75b30b99ae6
Romain Diegoni <romain_diegoni@hotmail.com>
2017-01-16 16:50:14 +0000

    Setup the liveupdate with action cable in the polymer element itself

--- app/assets/components/display-comment/display-comment.js
+++ app/assets/components/display-comment/display-comment.js
@@ -1,17 +1,37 @@
+function initializeRealtime(polymer) {
+  if (polymer.realtime === 'actioncable') {
+    initializeActionCable(polymer, "CommentChannel");
+  }
+};
+
+function initializeActionCable(polymer, channelName) {
+  App.comment = App.cable.subscriptions.create(channelName, {
+    connected: function() {
+      console.log("Connected to the channel CommentChannel");
+    },
+    disconnected: function() {
+      console.log("Disconnected to the channel CommentChannel");
+    },
+    received: function(data) {
+      polymer.push('comments', data.comment);
+    }
+  });
+};
+
 Polymer({
   is: "display-comment",
   properties: {
-    url: String
+    url: String,
+    realtime: String
   },
   ready: function() {
+    initializeRealtime(this);
+
     this.$.requestComments.url = this.url + "/comments";
     this.$.requestComments.generateRequest();
   },
   handleResponse: function (data) {
     this.comments = data.detail.response;
-  },
-  add: function(comment) {
-    this.push('comments', comment);
   },
   _format: function(date) {
     return moment(date).format('MMMM Do YYYY, h:mm a');
