fb257e3253b980693d36341e6a7c2e130e302146
drioemgaoin <romain_diegoni@hotmail.com>
2017-03-07 17:33:09 +0000

    Fix debug log error about the component render

--- app/assets/components/react/display-comment/display-comment.js.jsx
+++ app/assets/components/react/display-comment/display-comment.js.jsx
@@ -22,13 +22,13 @@ var DisplayComment = React.createClass({
   render: function() {
     var items = this.state.items.map((item) => {
       return (
-        <div class="comment">
+        <div key={item.id} class="comment">
           <div class="comment-avatar"><img src={this.getImage(item.avatar)} /></div>
           <div class="comment-infos">
             <span class="username">{item.username}</span>
             <span class="created-at">{this.format(item.created_at)}</span>
           </div>
-          <textarea>{item.content}</textarea>
+          <textarea defaultValue={item.content}></textarea>
         </div>
       )
     });
