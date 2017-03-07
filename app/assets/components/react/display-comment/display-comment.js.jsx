e8c0545770f1d15368995f56acd11c3b86dfd51c
drioemgaoin <romain_diegoni@hotmail.com>
2017-03-07 17:56:02 +0000

    Fix console debug issues and rewrite react component with ES6 syntax

--- app/assets/components/react/display-comment/display-comment.js.jsx
+++ app/assets/components/react/display-comment/display-comment.js.jsx
@@ -1,32 +1,33 @@
-var DisplayComment = React.createClass({
-  getInitialState: function() {
-    return {
+class DisplayComment extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
       url: this.props.url,
       defaultAvatar: this.props.defaultAvatar,
       items: []
-    }
-  },
+    };
+  }
 
-  componentDidMount: function() {
+  componentDidMount() {
     $.getJSON(this.state.url + '/comments', (response) => { this.setState({ items: response }) });
-  },
+  }
 
-  format: function(date) {
+  format(date) {
     return moment(date).format("MMMM Do YYYY, h:mm a");
-  },
+  }
 
-  getImage: function(avatar) {
+  getImage(avatar) {
     return avatar !== null ? avatar.thumb : this.state.defaultAvatar;
-  },
+  }
 
-  render: function() {
+  render() {
     var items = this.state.items.map((item) => {
       return (
-        <div key={item.id} class="comment">
-          <div class="comment-avatar"><img src={this.getImage(item.avatar)} /></div>
-          <div class="comment-infos">
-            <span class="username">{item.username}</span>
-            <span class="created-at">{this.format(item.created_at)}</span>
+        <div key={item.id} className="comment">
+          <div className="comment-avatar"><img src={this.getImage(item.avatar)} /></div>
+          <div className="comment-infos">
+            <span className="username">{item.username}</span>
+            <span className="created-at">{this.format(item.created_at)}</span>
           </div>
           <textarea defaultValue={item.content}></textarea>
         </div>
@@ -39,4 +40,4 @@ var DisplayComment = React.createClass({
         </div>
     )
   }
-});
+}
